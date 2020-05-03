afe23dc38c01fc3973b92cb8fcf45c2d6bdfb36a
JeromeTan1029 <64115240+JeromeTan1029@users.noreply.github.com>
2020-05-03 12:56:17 +0800

    Completed Week 2 Practical

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,118 @@
-package sg.edu.np.WhackAMole;
+package sg.edu.np.whack_a_mole;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* Hint
-        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 2.
-        - Feel free to modify the function to suit your program.
-    */
+    private Button ButtonLeft;
+    private Button ButtonMiddle;
+    private Button ButtonRight;
+    private TextView ScoreText;
+    private List<Button> buttonList = new ArrayList<Button>();
+    private int score;
+
+    private static final String TAG = "Whack-A-Mole";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setTitle("Whack-A-Mole!"); // Sets Title Of App
+
+        ButtonLeft = (Button) findViewById(R.id.ButtonLeft);
+        ButtonMiddle = (Button) findViewById(R.id.ButtonMiddle);
+        ButtonRight = (Button) findViewById(R.id.ButtonRight);
+        ScoreText = findViewById(R.id.Score);
+        score = 0;
+
+        buttonList.add(ButtonLeft);
+        buttonList.add(ButtonMiddle);
+        buttonList.add(ButtonRight);
+
+        ButtonLeft.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Log.v(TAG, "Left Button Clicked!");
+                if (hitCorrectButton(ButtonLeft)){
+                    Log.v(TAG, "Hit, score added!");
+                    score += 1;
+                }
+                else{
+                    Log.v(TAG, "Missed, score deducted!");
+                    score -= 1;
+                }
+                ScoreText.setText(score + "");
+                setNewMole(buttonList);
+            }
+        });
+
+        ButtonMiddle.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Log.v(TAG, "Middle Button Clicked!");
+                if (hitCorrectButton(ButtonMiddle)){
+                    Log.v(TAG, "Hit, score added!");
+                    score += 1;
+                }
+                else{
+                    Log.v(TAG, "Missed, score deducted!");
+                    score -= 1;
+                }
+                ScoreText.setText(score + "");
+                setNewMole(buttonList);
+            }
+        });
+
+        ButtonRight.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Log.v(TAG, "Right Button Clicked!");
+                if (hitCorrectButton(ButtonRight)){
+                    Log.v(TAG, "Hit, score added!");
+                    score += 1;
+                }
+                else{
+                    Log.v(TAG, "Missed, score deducted!");
+                    score -= 1;
+                }
+                ScoreText.setText(score + "");
+                setNewMole(buttonList);
+            }
+        });
 
         Log.v(TAG, "Finished Pre-Initialisation!");
     }
 
-    @Override
     protected void onStart(){
         super.onStart();
-        setNewMole();
-        Log.v(TAG, "Starting GUI!");
+        setNewMole(buttonList);
     }
 
+    private boolean hitCorrectButton(Button button){
+        if (button.getText().equals("*")){
+            return true;
+        }
 
-    public void setNewMole()
-    {
-        Random ran = new Random();
-        int randomLocation = ran.nextInt(3);
+        else if (button.getText().equals("O")){
+            return false;
+        }
+
+        return false;
+    }
+
+    private void setNewMole(List<Button> buttonList){
+        Random r = new Random();
+        int molePlace = r.nextInt(3);
+        for (Button button : buttonList){
+            button.setText("O");
+        }
+        buttonList.get(molePlace).setText("*");
     }
 }
